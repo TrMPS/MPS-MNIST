89fa6dc4aa4ff4d703eefe5d5d431df16defc393
Irene-Li <gnituyil@gmail.com>
2017-06-27 11:37:34 +0100

    Update to model.py
    # Conflicts:
    #	model.py

--- model.py
+++ model.py
@@ -4,16 +4,22 @@ import numpy as np
 '''
 TODO: 
 1. Make sure that input_size is big enough for the calculation to be meaningful
+2. Re-think how to read in features 
 '''
 
 # Model parameters
 input_size = 10
 d_feature = 2 
-d_matrix = 5
-d_output = 3 
+d_matrix = 4
+d_output = 3
 
-# Initialise the nodes and the inputs
-phi = [tf.Variable([1.0, 0], dtype=tf.float32)] * input_size # input, Phi_s, shape = d_feature 
+# Make up an input and an output 
+feature = [1.0, 0] # dim = d_feature 
+output = [1.0, 0, 0] # dim = d_output
+
+# Initialise the nodes, the input and the output
+phi = [tf.Variable(feature, dtype=tf.float32)] * input_size # input, Phi_s, shape = d_feature 
+delta = tf.Variable(output, dtype=tf.float32)
 nodes = [] # MPS, A^(alpha, beta)_s
 
 # A function for generating nodes
@@ -35,28 +41,27 @@ nodes.append(make_random_normal([d_matrix, d_feature]))
 
 
 # Calculate the first B 
-B = tf.einsum('ijml,jkn->ikmnl', nodes[1], nodes[2]) # ik: matrix leg, mn: input leg, l: output leg
+B = tf.einsum('ijml,jkn->lmnik', nodes[1], nodes[2]) # ik: matrix leg, mn: input leg, l: output leg
 
 # Calculate the first C, where f_l = B_ikmnl * C_ikmn
 C_1 = tf.einsum('in,n->i', nodes[0], phi[0]) # left part
 
-
-C_2 = tf.einsum('ijm,m->ij', nodes[3], phi[3]) # need to check whether the input size is large enough! 
+C_2 = tf.einsum('ijm,m->ij', nodes[3], phi[3]) 
 for i in range(4, input_size-1):
 	C_2 = tf.einsum('ij,jkm,m->ik', C_2, nodes[i], phi[i])
 C_2 = tf.einsum('ij,jm,m->i', C_2, nodes[-1], phi[-1])
+C = tf.einsum('i,k,m,n->mnik', C_1, C_2, phi[1], phi[2])
 
-C = tf.einsum('i,k,m,n->ikmn', C_1, C_2, phi[1], phi[2])
-
-
-# Calculate the decision function for one data point
-f = tf.einsum('ikmnl,ikmn->l', B, C)
+# Update B 
+f = tf.einsum('lmnik,mnik->l', B, C)
+B += tf.einsum('l,mnik->lmnik', delta - f, C)
+S, U, V = tf.svd(B, full_matrices=True)
 
 
 # Try 
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
-	print(sess.run(f))
+	print(sess.run())
 	
 
 
