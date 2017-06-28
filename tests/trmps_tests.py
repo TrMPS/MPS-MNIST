d011e9b118c28d1f2968f9d457c1d36dc2e4feca
Austen Lamacraft <al200@cam.ac.uk>
2017-06-28 14:04:05 +0100

    Testing tests

--- tests/trmps_tests.py
+++ tests/trmps_tests.py
@@ -6,6 +6,6 @@ import trmps.mps as mps
 
 class MPSTest(unittest.TestCase):
     def test_correct_dimensions_of_phi(self):
-        self.state = mps.MPS(2, 2, 10, 10, 10)
+        self.state = mps.MPS(2, 2, 5, 5, 5)
         self.state.prepare()
-        self.assertEqual(self.state.phi.shape.dims[0].value, 10)
+        self.assertEqual(self.state.phi.shape.dims[0].value, 5)
