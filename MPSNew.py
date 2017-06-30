6767f53d2acb09c1c81e5fdd5de2b0f1d5d17a02
Irene-Li <gnituyil@gmail.com>
2017-06-30 17:07:51 +0100

    Implementation of the predict function
    Currently not working

--- MPSNew.py
+++ MPSNew.py
@@ -11,7 +11,6 @@ class MPS(object):
         self.d_matrix = d_matrix
         self.d_feature = d_feature
         self.d_output = d_output
-        self.nodes = []
 
         # Initialise the nodes, input and output
         self._setup_nodes()
@@ -34,12 +33,54 @@ class MPS(object):
     def _make_random_normal(self, shape, mean=0, stddev=1):
         return tf.Variable(tf.random_normal(shape, mean=mean, stddev=stddev))
 
-    def predict(self, input):
+    def predict(self, feature):
+        phi = tf.placeholder(tf.float32, shape=[self.input_size, None, self.d_output])
+        f = self._predict(phi)
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            print(sess.run(f, {phi: feature}))
+            
+    def _predict(self, phi):
         """
-
-        :param input:
+        :param phi:
         :return:
         """
+        # Read in the nodes 
+        node1 = self.nodes.read(0)
+        node1.set_shape([self.d_feature, None])
+        node2 = self.nodes.read(1)
+        node2.set_shape([self.d_output, self.d_feature, None, None])
+        nodelast = self.nodes.read(self.input_size-1)
+        nodelast.set_shape([self.d_feature, None])
+
+        # Calculate C1 
+        C1 = tf.einsum('ni,tn->ti', node1, phi[0])
+        contracted_node2 = tf.einsum('lnij,tn->tlij', node2, phi[1])
+        C1 = tf.einsum('ti,tlij->tlj', C1, contracted_node2)
+
+        # Calculate C2
+        C2 = tf.einsum('mi,tm->ti', nodelast, phi[self.input_size-1])
+
+        #counter = tf.Variable(2, dtype=tf.int32)
+        cond = lambda counter, b: tf.less(counter, self.input_size-1)
+        _, C1 = tf.while_loop(cond=cond, body=self._chain_multiply, loop_vars=[2, C1], 
+                                        shape_invariants=[tf.TensorShape([]), tf.TensorShape([None, self.d_output, None])])
+        f = tf.einsum('tli,ti->tl', C1, C2)
+        return f 
+
+    def _chain_multiply(self, counter, C1):
+        node = self.nodes.read(counter)
+        node.set_shape([self.d_feature, None, None])
+        print(counter.dtype)
+        input_leg = phi[counter]
+        contracted_node = tf.einsum('mij,tm->tij', node, input_leg)
+        C1 = tf.einsum('tli,tij->tlj', C1, node)
+        counter = counter + 1 
+        return [counter, C1]
+
+
+
+
 
 
 class MPSOptimizer(object):
@@ -191,5 +232,5 @@ if __name__ == '__main__':
 
     # Initialise the model
     network = MPS(d_matrix, d_feature, d_output, input_size)
-    optimizer = MPSOptimizer(network, m, None, rate_of_change=rate_of_change)
-    optimizer.train(phi,delta)
+    network.predict(phi)
+    
