d209f8ed0ff2ec90c90d05974948c989e21d97ba
Irene-Li <Irene-Li@users.noreply.github.com>
2017-07-03 11:55:26 +0100

    Style changes

--- MPSNew.py
+++ MPSNew.py
@@ -58,6 +58,8 @@ class MPS(object):
 
             
 	def _predict(self, phi):
+
+		# Read in phi 
 		self.phi = phi
 
 		# Read in the nodes 
@@ -78,20 +80,17 @@ class MPS(object):
 		C2 = tf.einsum('mi,tm->ti', nodelast, phi[self.input_size-1])
 
         #counter = tf.Variable(2, dtype=tf.int32)
-		counter = 2
+		counter = 2 
 		cond = lambda c, b: tf.less(c, self.input_size-1)
 		_, C1 = tf.while_loop(cond=cond, body=self._chain_multiply, loop_vars=[counter, C1], 
 		                                shape_invariants=[tf.TensorShape([]), tf.TensorShape([None, self.d_output, None])])
-
 		f = tf.einsum('tli,ti->tl', C1, C2)
 		return f 
 
 	def _chain_multiply(self, counter, C1):
 		node = self.nodes.read(counter)
 		node.set_shape([self.d_feature, None, None])
-
 		input_leg = self.phi[counter]
-
 		contracted_node = tf.einsum('mij,tm->tij', node, input_leg)
 		C1 = tf.einsum('tli,tij->tlj', C1, contracted_node)
 		counter = counter + 1 
@@ -136,29 +135,32 @@ class MPSOptimizer(object):
         C2 = tf.einsum('mi,tm->ti', nlast, phi[-1])
         C2s = C2s.write(self.MPS.input_size-4, C2)
         cond = lambda counter,b,c: tf.less(counter, self.MPS.input_size-4)
-        # C2_finder = self._generate_C2_finder(nodes,phi)
         _, _, self.C2s = tf.while_loop(cond=cond, body=self._find_C2, loop_vars=[0, C2, C2s], 
                                         shape_invariants=[tf.TensorShape([]), tf.TensorShape([None, None]), 
                                         tf.TensorShape(None)])
+
+        # Check if this is the same 
 
     def _setup_training_graph(self):
         """
         Sets up graph needed to train the MPS, only for going to the right once.
         :return: nothing
         """
+
         counter = tf.Variable(0)
         updated_nodes = tf.TensorArray(tf.float32, size=self.MPS.input_size, dynamic_size=True, infer_shape=False)
         updated_nodes = updated_nodes.write(0, self.MPS.nodes.read(0))
-        update_func = self._generate_update_func(self.MPS.nodes, self._phi, self._delta, self.rate_of_change)
+
         n1 = self.MPS.nodes.read(1)
         n1.set_shape([None, None, None, None])
-        wrapped = [counter, self.C1, self.C2s,
-                   updated_nodes, n1]
+        self.C1s = tf.TensorArray(tf.float32, size=self.MPS.input_size-3, infer_shape=False)
+        wrapped = [counter, self.C1, self.C1s, self.C2s, updated_nodes, n1]
         cond = lambda counter, b, c, d, e: tf.less(counter, self.MPS.input_size - 3)
-        _, self.C1, self.C2s, self.updated_nodes, _ = tf.while_loop(cond=cond, body=update_func, loop_vars=wrapped,
+        _, self.C1, self.C2s, self.updated_nodes, _ = tf.while_loop(cond=cond, body=self._update, loop_vars=wrapped,
                                                                     parallel_iterations = 1,
                                                                     shape_invariants=[tf.TensorShape([]),
                                                                                       tf.TensorShape([None, None]),
+                                                                                      tf.TensorShape([None])
                                                                                       tf.TensorShape(None),
                                                                                       tf.TensorShape(None),
                                                                                       tf.TensorShape(
@@ -176,70 +178,63 @@ class MPSOptimizer(object):
         C2s = C2s.write(self.MPS.input_size-5-counter, new_C2)
         return [updated_counter, new_C2, C2s]
         
-
-    def _generate_update_func(self, nodes, phi, delta, rate):
         
-        def _update(counter, C1, C2s, updated_nodes, previous_node):
+    def _update(counter, C1, C1s, C2s, updated_nodes, previous_node):
 
-            # Read in the notes 
-            n1 = previous_node
-            n2 = nodes.read(counter+2)
-            n1.set_shape([self.MPS.d_output, self.MPS.d_feature, None, None])
-            n2.set_shape([self.MPS.d_feature, None, None])
+        # Read in the notes 
+		n1 = previous_node
+		n2 = self.MPS.nodes.read(counter+2)
+		n1.set_shape([self.MPS.d_output, self.MPS.d_feature, None, None])
+		n2.set_shape([self.MPS.d_feature, None, None])
 
-            # Calculate the bond 
-            bond = tf.einsum('lmij,njk->lmnik', n1, n2)
+        # Calculate the bond 
+        bond = tf.einsum('lmij,njk->lmnik', n1, n2)
 
-            # Calculate the C matrix 
-            C2 = C2s.read(counter)
-            C2.set_shape([None,None])
-            C = tf.einsum('ti,tk,tm,tn->tmnik', C1, C2, phi[counter], phi[counter+1])
+        # Calculate the C matrix 
+        C2 = C2s.read(counter)
+        C2.set_shape([None,None])
+        C = tf.einsum('ti,tk,tm,tn->tmnik', C1, C2, self._phi[counter], self._phi[counter+1])
 
-            # Update the bond 
-            f = tf.einsum('lmnik,tmnik->tl', bond, C)
-            gradient = tf.einsum('tl,tmnik->lmnik', delta - f, C)
-            delta_bond = rate * gradient
-            updated_bond = tf.add(bond, delta_bond)
+        # Update the bond 
+        f = tf.einsum('lmnik,tmnik->tl', bond, C)
+        gradient = tf.einsum('tl,tmnik->lmnik', self._delta - f, C)
+        delta_bond = self.rate_of_change * gradient
+        updated_bond = tf.add(bond, delta_bond)
 
-            # Decompose the bond 
-            aj, aj1 = self._bond_decomposition(updated_bond, m)
+    	# Decompose the bond 
+        aj, aj1 = self._bond_decomposition(updated_bond, m)
 
-            # Update and return the values
-            updated_nodes = updated_nodes.write(counter, aj)
-            contracted_aj = tf.einsum('mij,tm->tij', aj, phi[counter])
-            C1 = tf.einsum('tij,ti->tj', contracted_aj, C1)
-            updated_counter = counter + 1 
-            return [updated_counter, C1, C2s, updated_nodes, aj1]
-    
-        return _update
+        # Update and return the values
+        updated_nodes = updated_nodes.write(counter, aj)
+        contracted_aj = tf.einsum('mij,tm->tij', aj, self._phi[counter])
+        C1 = tf.einsum('tij,ti->tj', contracted_aj, C1)
+        updated_counter = counter + 1 
+
+        return [updated_counter, C1, C1s C2s, updated_nodes, aj1]
 
     def _bond_decomposition(self, bond, m):
         """
-        Decomposes bond, so that the next step can be done.
         :param bond:
         :param m:
         :return:
         """
         bond_reshaped = tf.transpose(bond, perm=[3, 1, 2, 0, 4])
-        dims = tf.shape(bond_reshaped)
-        l_dim = dims[0] * dims[1]
-        r_dim = dims[2] * dims[3] * dims[4]
-        bond_flattened = tf.reshape(bond_reshaped, [l_dim, r_dim])
+        bond_flattened = tf.reshape(bond_reshaped, [10, 30])
         s, u, v = tf.svd(bond_flattened)
         # filtered_s = s[:,-1]
         filtered_s = s
         s_size = tf.size(filtered_s)
         s_im = tf.reshape(tf.diag(filtered_s), [s_size, s_size, 1])
-        v_im = tf.reshape(v, [s_size, r_dim, 1])
-        u_im = tf.reshape(u, [l_dim, s_size, 1])
+        v_im = tf.reshape(v, [s_size, 30, 1])
+        u_im = tf.reshape(u, [10, s_size, 1])
         s_im_cropped = tf.image.resize_image_with_crop_or_pad(s_im, m, m)
-        v_im_cropped = tf.image.resize_image_with_crop_or_pad(v_im, m, r_dim)
-        u_im_cropped = tf.image.resize_image_with_crop_or_pad(u_im, l_dim, m)
+        v_im_cropped = tf.image.resize_image_with_crop_or_pad(v_im, m, 30)
+        u_im_cropped = tf.image.resize_image_with_crop_or_pad(u_im, 10, m)
         s_mat = tf.reshape(s_im_cropped, [m, m])
-        v_mat = tf.reshape(v_im_cropped, [m, r_dim])
-        a_prime_j_mixed = tf.reshape(u_im_cropped, [dims[0], dims[1], m])
+        v_mat = tf.reshape(v_im_cropped, [m, 30])
+        a_prime_j_mixed = tf.reshape(u_im_cropped, [5, 2, m])
         sv = tf.matmul(s_mat, v_mat)
-        a_prime_j1_mixed = tf.reshape(sv, [m, dims[2], dims[3], dims[4]])
+        a_prime_j1_mixed = tf.reshape(sv, [m, 2, 3, 5])
         a_prime_j = tf.transpose(a_prime_j_mixed, perm=[1, 0, 2])
         a_prime_j1 = tf.transpose(a_prime_j1_mixed, perm=[2, 1, 0, 3])
         return (a_prime_j, a_prime_j1)
@@ -247,9 +242,9 @@ class MPSOptimizer(object):
 if __name__ == '__main__':
     # Model parameters
     input_size = 4
-    d_feature = 2
+    d_feature = 5
     d_matrix = 5
-    d_output = 6
+    d_output = 3
     rate_of_change = 0.2
     batch_size = 10
     m = 5
@@ -257,12 +252,9 @@ if __name__ == '__main__':
     # Make up input and output
     phi = np.random.normal(size=(input_size, batch_size, d_feature)).astype(np.float32)
 
-    delta = []
-    for i in range(batch_size):
-        delta.append([0,1,0, 0, 0, 0])
+    delta = [[1, 0, 0], [0, 1, 0]]
 
     # Initialise the model
     network = MPS(d_matrix, d_feature, d_output, input_size)
-    optimizer = MPSOptimizer(network, m, None, rate_of_change = rate_of_change)
-    optimizer.train(phi, delta)
+    network.predict(phi)
     
