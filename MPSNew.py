83925bcc7ced3eb33a9964922f8b19607bc4ec99
Irene-Li <gnituyil@gmail.com>
2017-07-03 10:36:17 +0100

    Predict function is working!

--- MPSNew.py
+++ MPSNew.py
@@ -5,82 +5,92 @@ from collections import namedtuple
 #TODO: REALLY needs some documentation
 
 class MPS(object):
-    def __init__(self, d_matrix, d_feature, d_output, input_size):
-        # structure parameters
-        self.input_size = input_size
-        self.d_matrix = d_matrix
-        self.d_feature = d_feature
-        self.d_output = d_output
+	'''
+	Class variables:
+		input_size: int
+		d_matrix: int
+		d_feature: int
+		d_output: int
+		nodes: tf.TensorArray
+	'''
 
-        # Initialise the nodes, input and output
-        self._setup_nodes()
+	def __init__(self, d_matrix, d_feature, d_output, input_size):
+		# structure parameters
+		self.input_size = input_size
+		self.d_matrix = d_matrix
+		self.d_feature = d_feature
+		self.d_output = d_output
 
-    def _setup_nodes(self):
+		# Initialise the nodes, input and output
+		self._setup_nodes()
 
-        self.nodes = tf.TensorArray(tf.float32, size = 0, dynamic_size= True,
-                                    clear_after_read= False, infer_shape= False)
-        # First node
-        self.nodes = self.nodes.write(0, self._make_random_normal([self.d_feature, self.d_matrix]))
-        # The Second node with output leg attached
-        self.nodes = self.nodes.write(1, self._make_random_normal([self.d_output, self.d_feature, self.d_matrix, self.d_matrix]))
-        # The rest of the matrix nodes
+	def predict(self, feature):
+		'''
+		feature must be numpy array of dtype float32
+		'''
+		phi = tf.placeholder(tf.float32, shape=[self.input_size, None, self.d_feature])
+		f = self._predict(phi)
+		with tf.Session() as sess:
+		    sess.run(tf.global_variables_initializer())
+		    print(sess.run(f, {phi: feature}))
 
-        for i in range(self.input_size - 3):
-            self.nodes = self.nodes.write(i+2, self._make_random_normal([self.d_feature, self.d_matrix, self.d_matrix]))
-        # Last node
-        self.nodes = self.nodes.write(self.input_size - 1, self._make_random_normal([self.d_feature, self.d_matrix]))
+	# ================
+	# hidden functions
+	# ================
 
-    def _make_random_normal(self, shape, mean=0, stddev=1):
-        return tf.Variable(tf.random_normal(shape, mean=mean, stddev=stddev))
+	def _setup_nodes(self):
 
-    def predict(self, feature):
-        phi = tf.placeholder(tf.float32, shape=[self.input_size, None, self.d_output])
-        f = self._predict(phi)
-        with tf.Session() as sess:
-            sess.run(tf.global_variables_initializer())
-            print(sess.run(f, {phi: feature}))
+		self.nodes = tf.TensorArray(tf.float32, size = 0, dynamic_size= True,
+		                            clear_after_read= False, infer_shape= False)
+		# First node
+		self.nodes = self.nodes.write(0, self._make_random_normal([self.d_feature, self.d_matrix]))
+		# The Second node with output leg attached
+		self.nodes = self.nodes.write(1, self._make_random_normal([self.d_output, self.d_feature, self.d_matrix, self.d_matrix]))
+		# The rest of the matrix nodes
+		for i in range(self.input_size - 3):
+		    self.nodes = self.nodes.write(i+2, self._make_random_normal([self.d_feature, self.d_matrix, self.d_matrix]))
+		# Last node
+		self.nodes = self.nodes.write(self.input_size-1, self._make_random_normal([self.d_feature, self.d_matrix]))
+
+	def _make_random_normal(self, shape, mean=0, stddev=1):
+		return tf.Variable(tf.random_normal(shape, mean=mean, stddev=stddev))
+
             
-    def _predict(self, phi):
-        """
-        :param phi:
-        :return:
-        """
-        # Read in the nodes 
-        node1 = self.nodes.read(0)
-        node1.set_shape([self.d_feature, None])
-        node2 = self.nodes.read(1)
-        node2.set_shape([self.d_output, self.d_feature, None, None])
-        nodelast = self.nodes.read(self.input_size-1)
-        nodelast.set_shape([self.d_feature, None])
+	def _predict(self, phi):
 
-        # Calculate C1 
-        C1 = tf.einsum('ni,tn->ti', node1, phi[0])
-        contracted_node2 = tf.einsum('lnij,tn->tlij', node2, phi[1])
-        C1 = tf.einsum('ti,tlij->tlj', C1, contracted_node2)
+		# Read in the nodes 
+		node1 = self.nodes.read(0)
+		node1.set_shape([self.d_feature, None])
+		node2 = self.nodes.read(1)
+		node2.set_shape([self.d_output, self.d_feature, None, None])
+		nodelast = self.nodes.read(self.input_size-1)
+		nodelast.set_shape([self.d_feature, None])
 
-        # Calculate C2
-        C2 = tf.einsum('mi,tm->ti', nodelast, phi[self.input_size-1])
+		# Calculate C1 
+		C1 = tf.einsum('ni,tn->ti', node1, phi[0])
+		contracted_node2 = tf.einsum('lnij,tn->tlij', node2, phi[1])
+		C1 = tf.einsum('ti,tlij->tlj', C1, contracted_node2)
+
+
+ 		# Calculate C2
+		C2 = tf.einsum('mi,tm->ti', nodelast, phi[self.input_size-1])
 
         #counter = tf.Variable(2, dtype=tf.int32)
-        cond = lambda counter, b: tf.less(counter, self.input_size-1)
-        _, C1 = tf.while_loop(cond=cond, body=self._chain_multiply, loop_vars=[2, C1], 
-                                        shape_invariants=[tf.TensorShape([]), tf.TensorShape([None, self.d_output, None])])
-        f = tf.einsum('tli,ti->tl', C1, C2)
-        return f 
+		counter = tf.constant(2)
+		cond = lambda c, b: tf.less(c, self.input_size-1)
+		_, C1 = tf.while_loop(cond=cond, body=self._chain_multiply, loop_vars=[counter, C1], 
+		                                shape_invariants=[counter.get_shape(), tf.TensorShape([None, self.d_output, None])])
+		f = tf.einsum('tli,ti->tl', C1, C2)
+		return f 
 
-    def _chain_multiply(self, counter, C1):
-        node = self.nodes.read(counter)
-        node.set_shape([self.d_feature, None, None])
-        print(counter.dtype)
-        input_leg = phi[counter]
-        contracted_node = tf.einsum('mij,tm->tij', node, input_leg)
-        C1 = tf.einsum('tli,tij->tlj', C1, node)
-        counter = counter + 1 
-        return [counter, C1]
-
-
-
-
+	def _chain_multiply(self, counter, C1):
+		node = self.nodes.read(counter)
+		node.set_shape([self.d_feature, None, None])
+		input_leg = tf.gather(phi, counter) # "phi[counter]"
+		contracted_node = tf.einsum('mij,tm->tij', node, input_leg)
+		C1 = tf.einsum('tli,tij->tlj', C1, contracted_node)
+		counter = counter + 1 
+		return [counter, C1]
 
 
 class MPSOptimizer(object):
@@ -218,7 +228,7 @@ class MPSOptimizer(object):
 
 if __name__ == '__main__':
     # Model parameters
-    input_size = 2
+    input_size = 4
     d_feature = 5
     d_matrix = 5
     d_output = 3
@@ -227,7 +237,8 @@ if __name__ == '__main__':
     m = 5
 
     # Make up input and output
-    phi = np.random.normal(size=(input_size, batch_size, d_feature))
+    phi = np.random.normal(size=(input_size, batch_size, d_feature)).astype(np.float32)
+
     delta = [[1, 0, 0], [0, 1, 0]]
 
     # Initialise the model
