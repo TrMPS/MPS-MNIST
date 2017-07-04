68b5c5f25332604f7d9b609b0d52a66860ef006e
Austen Lamacraft <al200@cam.ac.uk>
2017-07-04 16:30:43 +0100

    Added write_graph() to MPS.py

--- trmps/mps.py
+++ trmps/mps.py
@@ -1,4 +1,4 @@
-import tensorflow as tf 
+import tensorflow as tf
 import numpy as np
 
 '''
@@ -18,7 +18,7 @@ class MPS(object):
     '''
 
     def __init__(self, d_matrix, d_feature, d_output, input_size):
-        
+
         # structure parameters
         self.input_size = input_size
         self.d_matrix = d_matrix
@@ -43,6 +43,10 @@ class MPS(object):
             test_accuracy = sess.run(accuracy, {feature:test_feature, label:test_label})
             print(test_cost)
             print(test_accuracy)
+
+    def write_graph(self, logdir = "output"):
+        writer = tf.summary.FileWriter(logdir, tf.get_default_graph())
+        writer.close()
 
     # ================
     # hidden functions
@@ -75,13 +79,13 @@ class MPS(object):
 
     def _make_random_normal(self, shape, mean=0, stddev=1):
         return tf.Variable(tf.random_normal(shape, mean=mean, stddev=stddev))
-            
+
     def predict(self, feature):
 
-        # Read in feature 
+        # Read in feature
         self.feature = feature
 
-        # Read in the nodes 
+        # Read in the nodes
         node1 = self.nodes.read(0)
         node1.set_shape([self.d_feature, None])
         node2 = self.nodes.read(1)
@@ -90,7 +94,7 @@ class MPS(object):
         nodelast = self.nodes.read(self.input_size-1)
         nodelast.set_shape([self.d_feature, None])
 
-        # Calculate C1 
+        # Calculate C1
         C1 = tf.einsum('ni,tn->ti', node1, feature[0])
         contracted_node2 = tf.einsum('lnij,tn->tlij', node2, feature[1])
         C1 = tf.einsum('ti,tlij->tlj', C1, contracted_node2)
@@ -99,12 +103,12 @@ class MPS(object):
         C2 = tf.einsum('mi,tm->ti', nodelast, feature[self.input_size-1])
 
         #counter = tf.Variable(2, dtype=tf.int32)
-        counter = 2 
+        counter = 2
         cond = lambda c, b: tf.less(c, self.input_size-1)
-        _, C1 = tf.while_loop(cond=cond, body=self._chain_multiply, loop_vars=[counter, C1], 
+        _, C1 = tf.while_loop(cond=cond, body=self._chain_multiply, loop_vars=[counter, C1],
                                         shape_invariants=[tf.TensorShape([]), tf.TensorShape([None, self.d_output, None])])
         f = tf.einsum('tli,ti->tl', C1, C2)
-        return f 
+        return f
 
     def _chain_multiply(self, counter, C1):
         node = self.nodes.read(counter)
@@ -112,7 +116,7 @@ class MPS(object):
         input_leg = self.feature[counter]
         contracted_node = tf.einsum('mij,tm->tij', node, input_leg)
         C1 = tf.einsum('tli,tij->tlj', C1, contracted_node)
-        counter = counter + 1 
+        counter = counter + 1
         return [counter, C1]
 
 if __name__ == '__main__':
@@ -130,12 +134,9 @@ if __name__ == '__main__':
     phi = np.random.normal(size=(input_size, batch_size, d_feature)).astype(np.float32)
 
     delta = np.zeros((batch_size, d_output))
-    delta[:500, 1] = 1 
+    delta[:500, 1] = 1
     delta[500:, 4] = 1
 
     # Initialise the model
     network = MPS(d_matrix, d_feature, d_output, input_size)
     network.test(phi, delta)
-    
-
-
