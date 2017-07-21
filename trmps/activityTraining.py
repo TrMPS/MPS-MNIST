6dd7d9ce0e9e8274a26b15f6f465f7f57968af13
Taketomo <ti250@cam.ac.uk>
2017-07-21 14:41:50 +0100

    Activity classification now works
    V. high accuracy

--- trmps/activityTraining.py
+++ trmps/activityTraining.py
@@ -4,14 +4,14 @@ import activitypreprocessing as ap
 # Model parameters
 d_feature = 4
 d_output = 7
-batch_size = 1000
+batch_size = 2000
 permuted = False
 shuffled = False
-input_size = 200
+input_size = 100
 
-max_size = 20
+max_size = 15
 
-rate_of_change = 10 ** (-7)
+rate_of_change = 10**(-7)
 logging_enabled = False
 
 cutoff = 10 # change this next
@@ -26,9 +26,9 @@ weights = None
 network = MPS(d_feature, d_output, input_size)
 network.prepare(data_source)
 feature, label = data_source.next_training_data_batch(1000)
-network.test(feature, label)
-# optimizer = MPSOptimizer(network, max_size, None, cutoff=cutoff)
-# optimizer.train(data_source, batch_size, n_step, 
-#                 rate_of_change=rate_of_change, 
-#                 logging_enabled=logging_enabled, 
-#                 initial_weights=weights)
+# network.test(feature, label)
+optimizer = MPSOptimizer(network, max_size, None, cutoff=cutoff)
+optimizer.train(data_source, batch_size, n_step, 
+                rate_of_change=rate_of_change, 
+                logging_enabled=logging_enabled, 
+                initial_weights=weights)
