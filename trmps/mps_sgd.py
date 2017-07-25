df0d68013f0f33ff654b2e1cba14e8042a5f31a6
Irene-Li <gnituyil@gmail.com>
2017-07-25 14:56:53 +0100

    SGD working!

--- trmps/mps_sgd.py
+++ trmps/mps_sgd.py
@@ -6,7 +6,7 @@ import MNISTpreprocessing
 
 class SimpleMPS(MPS):
 
-    def __init__(self, d_feature, d_output, input_size):
+    def __init__(self, d_feature, d_output, input_size, feature_reg=1.1, reg=0.001):
         """
         Initialises the MPS. Currently, the prepare method must be called immediately
         after this before anything else can be done.
@@ -22,17 +22,38 @@ class SimpleMPS(MPS):
             The input size, i.e. the number of matrices composing the matrix product state
         """
         # structure parameters
+        self.feature_reg = feature_reg
+        self.reg = reg
+
         super().__init__(d_feature, d_output, input_size)
 
     def predict(self, feature):
         C1 = tf.matmul(feature[0], self.nodes[0])
-        C1 = tf.Print(C1, [C1])
-        for i in range(self.input_size-1):
+        for i in range(1, self.input_size-1):
             node = self.nodes[i] 
             contracted_node = tf.einsum('tn,nij->tij', feature[i], node)
             C1 = tf.einsum('ti,tij->tj', C1, contracted_node)
-        C2 = tf.matmul(feature[-1], self.nodes[-1])
-        return tf.einsum('ti,til->l', C1, C2)
+        C2 = tf.einsum('tn,njl->tjl', feature[-1], self.nodes[-1])
+        return tf.einsum('ti,til->tl', C1, C2)
+
+    def regularisation(self):
+        reg_weights = tf.constant([1] + [self.feature_reg] * (self.d_feature - 1),
+                                  dtype=tf.float32)
+        reg_node = tf.einsum('ni,n->ni', self.nodes[0], reg_weights)
+        penalty = tf.tensordot(reg_node, reg_node, [[0], [0]])
+
+        for i in range(1, self.input_size-1):
+            node = self.nodes[i]
+            reg_node = tf.einsum('nij,n->nij', self.nodes[i], reg_weights)
+            contracted_node = tf.tensordot(reg_node, reg_node, [[0], [0]])
+            penalty = tf.einsum('ij,ikjl->kl', penalty, contracted_node)
+        penalty = tf.reduce_sum(tf.multiply(penalty, penalty))
+        return self.reg * tf.squeeze(penalty)
+
+    def cost(self, f, label):
+        loss = tf.reduce_mean((f - label)**2)
+        reg_penalty = self.regularisation() 
+        return loss + reg_penalty
 
     def _setup_nodes(self):
         """
@@ -57,7 +78,7 @@ class SimpleMPS(MPS):
         start_vector = np.zeros([self.d_feature, self.d_matrix], dtype=np.float32)
         start_vector[0, self.d_output:] = 1
         start_vector[1:, 0:self.d_output] = self.weight[0]
-        return tf.Variable(start_vector, dtype=tf.float32)
+        return tf.Variable(start_vector, dtype=tf.float32, trainable=True, name='start')
 
     def _make_end_vector(self):
         """
@@ -72,7 +93,7 @@ class SimpleMPS(MPS):
         end_vector[0, self.d_output:, :] = np.diag(self.bias)
         end_vector[1:, self.d_output:, :] = np.diag(self.weight[-1])
 
-        return tf.Variable(end_vector, dtype=tf.float32)
+        return tf.Variable(end_vector, dtype=tf.float32, trainable=True, name='end')
 
     def _make_middle_node(self, index):
         """
@@ -86,7 +107,7 @@ class SimpleMPS(MPS):
         for i in range(1, self.d_feature):
             middle_node[i, self.d_output:, 0:self.d_output] = np.diag(self.weight[index, i-1])
 
-        return tf.Variable(middle_node, dtype=tf.float32)
+        return tf.Variable(middle_node, dtype=tf.float32, trainable=True, name='G_{}'.format(i))
 
 if __name__ == '__main__':
     # Model parameters
--- trmps/sgd_optimizer.py
+++ trmps/sgd_optimizer.py
@@ -1,29 +1,69 @@
 import tensorflow as tf
 import time
-from mps import MPS
-import pickle
+from mps_sgd import SimpleMPS
 import utils
 from tensorflow.python.client import timeline
-from optimizer import MPSOptimizer
+from MNISTpreprocessing import MNISTDatasource
 
-class SDGOptimizer(object):
+class SGDOptimizer(object):
 
-	def __init__(self):
-		self.MPS = MPSNetwork
-        self.rate_of_change = tf.placeholder(tf.float32, shape=[])
-        self.max_size = max_size
-        self.grad_func = grad_func
-        self.cutoff = cutoff
-        self._feature = tf.placeholder(tf.float32, shape=[self.MPS.input_size, None, self.MPS.d_feature])
-        self._label = tf.placeholder(tf.float32, shape=[None, self.MPS.d_output])
-        self._setup_optimization()
-        _ = self.train_step()
+    def __init__(self, network):
+        self.MPS = network
+
+    def train(self, data_source, batch_size, n_steps, rate_of_change=0.001):
+        features = tf.placeholder(tf.float32, shape=[self.MPS.input_size, None, self.MPS.d_feature])
+        labels = tf.placeholder(tf.float32, shape=[None, self.MPS.d_output])
+
+        f = self.MPS.predict(features)
+        cost = self.MPS.cost(f, labels)
+        accuracy = self.MPS.accuracy(f, labels)
+        conf_mat = self.MPS.confusion_matrix(f, labels)
+        trainer = tf.train.AdamOptimizer(rate_of_change).minimize(cost)
+
+        to_eval = [accuracy, cost, conf_mat]
+
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+
+            for i in range(n_steps):
+                batch_feature, batch_label = data_source.next_training_data_batch(batch_size)
+                feed_dict = {features: batch_feature, labels: batch_label}
+                _ = sess.run(trainer, feed_dict=feed_dict)
+                if i % 10 == 0:
+                    train_acc, train_cost, train_conf_mat = sess.run(to_eval, feed_dict=feed_dict)
+                    print('step {}, accuracy {}'.format(i, train_acc))
+                    print('step {}, training cost {}'.format(i, train_cost))
+                    print(train_conf_mat)
+
+            test_feature, test_label = data_source.test_data
+            feed_dict = {features: test_feature, labels: test_label}
+            to_eval = [accuracy, cost, conf_mat]
+            sess.run(to_eval, feed_dict=feed_dict)
+
+if __name__ == '__main__':
+    # Model parameters
+    d_feature = 2
+    d_output = 10
+    batch_size = 100
+    permuted = False
+    shuffled = True
+    shrink = True
+    input_size = 784
+    if shrink:
+            input_size = 196
+
+    rate_of_change = 0.0001
+    n_step = 1000
+
+    data_source = MNISTDatasource(shrink=shrink, permuted=permuted, shuffled=shuffled)
+
+    weights = None
+
+    network = SimpleMPS(d_feature, d_output, input_size)
+    network.prepare(data_source)
+    optimizer = SGDOptimizer(network)
+    optimizer.train(data_source, batch_size, n_step, 
+                    rate_of_change=rate_of_change)
 
 
-    def _setup_optimization(self):
-    	"""
-    	load all the nodes from placeholder to a list
-    	"""
-    	
-
-    def train_step(): 
+        
