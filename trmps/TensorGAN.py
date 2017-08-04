a1749fa518eba950f770039966ec00ea220ff001
Taketomo <ti250@cam.ac.uk>
2017-08-04 15:10:12 +0100

    Committing current work on TensorGAN before re-architecting

---
+++ trmps/TensorGAN.py
@@ -0,0 +1,7 @@
+from mps import MPS
+import numpy as np
+import tensorflow as tensorflow
+
+class GeneratorMPS(MPS):
+    def create(seed):
+        return (super().predict(seed), tf.ones([]) #ACTUALLY IMPLEMENT
