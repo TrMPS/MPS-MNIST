d50b0eab102e56cf70db8e8077802976acb5a0b4
Irene-Li <gnituyil@gmail.com>
2017-08-16 17:12:11 +0100

    Revert the preprocessing to what it was

--- trmps/MNISTTraining.py
+++ trmps/MNISTTraining.py
@@ -1,12 +1,11 @@
 from optimizer import *
 import MNISTpreprocessing
-from squaredDistanceMPS import *
+from mps import * 
 
 # Model parameters
 d_feature = 2
 d_output = 10
 input_size = 784
-lin_reg_learning_rate = 10**(-4)
 
 # Data parameters
 permuted = False
@@ -14,24 +13,22 @@ shuffled = True
 shrink = True
 if shrink:
     input_size = 196
-
-special_node_loc = 98
+special_node_loc = 91
 
 # Optimizer parameters
-batch_size = 2000
-max_size = 30
-min_singular_value = 0.001
-reg = 0.01
-armijo_coeff = 10**(-1)
+batch_size = 10000
+max_size = 20 
+min_singular_value = 1e-8
+reg = 1e-2
+use_hessian = False
 
-rate_of_change = 5 * 10 ** (-4)
-lr_reg = 0.0
-
+rate_of_change = 1e-5
+lr_reg = 0.0 
 logging_enabled = False
-verbosity = -0
+verbosity = 50000
 
 cutoff = 100
-n_step = 6
+n_step = 7
 
 data_source = MNISTpreprocessing.MNISTDatasource(shrink=shrink, permuted=permuted, shuffled=shuffled)
 
@@ -44,13 +41,11 @@ data_source = MNISTpreprocessing.MNISTDatasource(shrink=shrink, permuted=permute
 
 weights=None
 optimizer_parameters = MPSOptimizerParameters(cutoff=cutoff, reg=reg, lr_reg=lr_reg,
-                                              verbosity=verbosity)
+                                              verbosity=verbosity, use_hessian=use_hessian)
 training_parameters = MPSTrainingParameters(rate_of_change=rate_of_change, initial_weights=weights,
                                             _logging_enabled=logging_enabled)
-
-network = sqMPS(d_feature, d_output, input_size, special_node_loc)
-network.prepare(data_source=data_source, learning_rate=lin_reg_learning_rate)
-optimizer = sqMPSOptimizer(network, max_size, optimizer_parameters)
+network = MPS(d_feature, d_output, input_size, special_node_loc)
+network.prepare(data_source=None)
+optimizer = MPSOptimizer(network, max_size, optimizer_parameters)
 optimizer.train(data_source, batch_size, n_step,
                 training_parameters)
-
