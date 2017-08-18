444fce98aba0766be53f67f3a27e0eb1b4f00152
Taketomo <ti250@cam.ac.uk>
2017-08-18 16:28:17 +0100

    Added functions to utils to plot a numpy array of snaked images

--- trmps/utils.py
+++ trmps/utils.py
@@ -3,26 +3,27 @@ import urllib
 import tarfile
 import sys
 import numpy as np
+import matplotlib.pyplot as plt
 
 def check_nan(tensor, name, replace_nan=True):
     s = tf.reduce_sum(tensor)
     is_nan = tf.is_nan(s)
-    tensor = tf.cond(is_nan, 
-                     true_fn=lambda: tf.Print(tensor, [tensor], 
-                                            message='{} is not finite'.format(name)), 
+    tensor = tf.cond(is_nan,
+                     true_fn=lambda: tf.Print(tensor, [tensor],
+                                            message='{} is not finite'.format(name)),
                      false_fn=lambda: tensor)
     if replace_nan:
-        tensor = tf.where(tf.is_nan(tensor), 
-                          tf.zeros_like(tensor), 
+        tensor = tf.where(tf.is_nan(tensor),
+                          tf.zeros_like(tensor),
                           tensor)
-    return tensor 
+    return tensor
 
 def getunzipped(url, name):
     try:
         name, hdrs = urllib.request.urlretrieve(url, name)
     except IOError as e:
         print('Cannot retrieve {}: {}'.format(url, e))
-        return 
+        return
     z = tarfile.open(name, "r:gz")
     z.extractall()
     z.close()
@@ -54,14 +55,14 @@ class spinner(object):
         self.jump = jump
         self.percentage = 0
         self.counter = 0
-        
+
     def print_spinner(self, percentage):
         if float(percentage) == 100.0:
             sys.stdout.flush()
             print("\r" + str(100) + " % done")
         elif self.index % self.jump == 0:
             sys.stdout.flush()
-            # Spinner to show progress 
+            # Spinner to show progress
             if self.counter == 0:
                 print("\r" + str(percentage) + " % done", end="|")
                 self.counter += 1
@@ -75,7 +76,7 @@ class spinner(object):
                 print("\r" + str(percentage) + " % done", end="\\")
                 self.counter = 0
         self.index += 1
-        
+
 # Adapted from https://stackoverflow.com/questions/29831489/numpy-1-hot-array
 def convert_to_onehot(vector, num_classes=None):
     """
@@ -106,7 +107,16 @@ def convert_to_onehot(vector, num_classes=None):
     result[np.arange(len(vector)), vector] = 1
     return result.astype(int)
 
+def into_image(snaked_image):
+    length = len(snaked_image)
+    dim = int(np.sqrt(length))
+    if float(dim) != float(np.sqrt(length)):
+        print("something's wrong; please pass in a snaked image that was originally square")
+    image = np.reshape(snaked_image, [dim, dim])
+    return image
 
+def show(snaked_image):
+    imgplot = plt.imshow(into_image(snaked_image), interpolation='none', cmap='binary')
 
 
 
