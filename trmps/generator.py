ef5b225b6a9639f21b4a499d718a91221e62bf57
Taketomo <ti250@cam.ac.uk>
2017-08-24 11:34:48 +0100

    Plots the average samples as well now

--- trmps/generator.py
+++ trmps/generator.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 import numpy as np
 from distribution import Quadratic
-from short_mps import * 
-import pickle 
+from short_mps import *
+import pickle
 import utils
 from matplotlib import pyplot as plt
 import MNISTpreprocessing
@@ -10,7 +10,7 @@ import MNISTpreprocessing
 class MPSGenerator(object):
 
     def __init__(self, MPSNetwork):
-        self.MPS = MPSNetwork 
+        self.MPS = MPSNetwork
         assert self.MPS.d_feature == 2
 
     def generate(self, n_samples, digit):
@@ -22,35 +22,35 @@ class MPSGenerator(object):
         loc = self.MPS._special_node_loc
         middle = self._sample_from_special_node()
         cond = lambda c, l, t, f: tf.less(c, self.MPS.input_size)
-        _, middle, self.samples_ta, _ = tf.while_loop(cond=cond, 
-                             body=self._sample_from_node, 
+        _, middle, self.samples_ta, _ = tf.while_loop(cond=cond,
+                             body=self._sample_from_node,
                              loop_vars=[loc+1, middle, self.samples_ta, True],
-                             shape_invariants=[tf.TensorShape([]), 
-                                               tf.TensorShape([None, None, None]), 
+                             shape_invariants=[tf.TensorShape([]),
+                                               tf.TensorShape([None, None, None]),
                                                tf.TensorShape(None),
-                                               tf.TensorShape([])], 
+                                               tf.TensorShape([])],
                              parallel_iterations=5)
 
 
         cond = lambda c, l, t, f: tf.greater_equal(c, 0)
-        _, middle, self.samples_ta, _ = tf.while_loop(cond=cond, 
-                                    body=self._sample_from_node, 
+        _, middle, self.samples_ta, _ = tf.while_loop(cond=cond,
+                                    body=self._sample_from_node,
                                     loop_vars=[loc-1, middle, self.samples_ta, False],
-                                    shape_invariants=[tf.TensorShape([]), 
-                                                       tf.TensorShape([None, None, None]), 
+                                    shape_invariants=[tf.TensorShape([]),
+                                                       tf.TensorShape([None, None, None]),
                                                        tf.TensorShape(None),
-                                                       tf.TensorShape([])], 
+                                                       tf.TensorShape([])],
                                     parallel_iterations=5)
 
-        
-        return self.samples_ta.stack() 
-    
+
+        return self.samples_ta.stack()
+
     def _sample_from_matrices(self, matrices):
         with tf.name_scope("recover_vectors"):
             vectors = tf.map_fn(lambda x:tf.diag_part(x), matrices)
             vectors = tf.sqrt(vectors)
             signs = tf.map_fn(lambda x:tf.sign(x[0]), matrices)
-            vectors = signs * vectors 
+            vectors = signs * vectors
 
         return self._sample_from_vectors(vectors)
 
@@ -59,7 +59,7 @@ class MPSGenerator(object):
             # vectors = tf.Print(vectors, [vectors[0]])
             dist = Quadratic(vectors[:, 0], vectors[:, 1])
             samples = dist.sample()
-            del dist 
+            del dist
 
         return samples
 
@@ -67,23 +67,22 @@ class MPSGenerator(object):
 
         with tf.name_scope("read_node"):
             node = self._read_node(counter)
-            node = tf.Print(node, [counter, tf.shape(node)])
 
         with tf.name_scope("sample_from_node"):
-            middle_dot_node = tf.cond(right_flag, 
-                                      true_fn=lambda: tf.einsum('tij,mjk->tmik', middle, node), 
+            middle_dot_node = tf.cond(right_flag,
+                                      true_fn=lambda: tf.einsum('tij,mjk->tmik', middle, node),
                                       false_fn=lambda: tf.einsum('tjk,mij->tmik', middle, node))
             contracted_node = tf.einsum('tmik,tnik->tmn', middle_dot_node, middle_dot_node)
             samples = self._sample_from_matrices(contracted_node)
-            samples_ta = samples_ta.write(counter, samples) 
+            samples_ta = samples_ta.write(counter, samples)
 
         with tf.name_scope("update_middle"):
             feature = self._preprocess(samples)
             middle = tf.einsum('tmik,tm->tik', middle_dot_node, feature)
 
         with tf.name_scope("update_counter"):
-            counter = tf.cond(right_flag, 
-                              true_fn=lambda: counter+1, 
+            counter = tf.cond(right_flag,
+                              true_fn=lambda: counter+1,
                               false_fn=lambda: counter-1)
 
         return counter, middle, samples_ta, right_flag
@@ -113,7 +112,7 @@ class MPSGenerator(object):
 
         node = tf.case([case1, case2], default=default)
 
-        return node 
+        return node
 
 
 
@@ -146,15 +145,15 @@ class MPSGenerator(object):
 
     def _normalise_special_node(self, special_node):
         special_node.set_shape([self.MPS.d_output, self.MPS.d_feature, None, None])
-        matrix_dim_1 = tf.shape(special_node)[-2] 
+        matrix_dim_1 = tf.shape(special_node)[-2]
         matrix_dim_2 = tf.shape(special_node)[-1]
         collective_dim = self.MPS.d_output * self.MPS.d_feature * matrix_dim_1
         reshaped_node = tf.reshape(special_node, [collective_dim, matrix_dim_2])
         s, u, v = tf.svd(reshaped_node)
-        s = s/tf.norm(s) 
+        s = s/tf.norm(s)
         u = u * tf.expand_dims(s, 0)
         special_node = tf.matmul(u, v, transpose_b=True)
-        special_node = tf.reshape(special_node, 
+        special_node = tf.reshape(special_node,
                                   [self.MPS.d_output, self.MPS.d_feature, matrix_dim_1, matrix_dim_2])
 
         return special_node
@@ -187,13 +186,13 @@ if __name__ == '__main__':
 
     generator = MPSGenerator(network)
 
-    digit = 1
+    digit = 3
     n_samples = 100
     samples = generator.generate(n_samples, digit)
 
     feature = tf.stack([tf.ones_like(samples), np.sqrt(3) * (2 * samples - 1)], axis=-1)
     label_np = np.zeros([n_samples, 10])
-    label_np[:, digit] = 1 
+    label_np[:, digit] = 1
     label = tf.constant(label_np)
 
     f = network.predict(feature)
@@ -204,7 +203,7 @@ if __name__ == '__main__':
     feed_dict = network.create_feed_dict(weights)
 
 
-    with tf.Session() as sess: 
+    with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
 
         to_eval = [samples, cost, accuracy, confusion]
@@ -214,7 +213,12 @@ if __name__ == '__main__':
         print('Pixels with values larger than 1: ')
         print(samples[samples > 1])
 
+    print(samples.shape)
     utils.show(samples[:, 0])
+    plt.figure()
+    avg_samples = np.mean(samples, axis=1)
+    print(avg_samples.shape)
+    utils.show(avg_samples)
     plt.show()
 
 
