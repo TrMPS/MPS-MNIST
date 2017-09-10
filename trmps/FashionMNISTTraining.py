4f3d15c76cc832b78125fb898ef0170a6bb17fd2
Taketomo <ti250@cam.ac.uk>
2017-09-10 22:51:53 +0900

    FashionMNISTTraining can now also test how well training did

--- trmps/FashionMNISTTraining.py
+++ trmps/FashionMNISTTraining.py
@@ -15,12 +15,12 @@ shrink = False
 if shrink:
     input_size = 196
 
-special_node_loc = None
+special_node_loc = 14
 
 # Optimizer parameters
 batch_size = 20000
 max_size = 25
-min_singular_value = 0.001
+min_singular_value = 0.00
 reg = 0.01
 armijo_coeff = 10**(-4)
 armijo_iterations = 25
@@ -35,24 +35,40 @@ cutoff = 100
 n_step = 6
 
 data_source = FashionMNISTDatasource(shrink=shrink, permuted=permuted, shuffled=shuffled)
-
-# Initialise the model
-
-# with open('weights_sgd', 'rb') as fp:
-#     weights = pickle.load(fp)
-#     if len(weights) != input_size:
-#         weights = None
-
-weights=None
-optimizer_parameters = MPSOptimizerParameters(cutoff=cutoff, reg=reg, lr_reg=lr_reg,
-                                              verbosity=verbosity, armijo_iterations=armijo_iterations)
-training_parameters = MPSTrainingParameters(rate_of_change=rate_of_change, initial_weights=weights,
-                                            _logging_enabled=logging_enabled)
-
 network = sqMPS(d_feature, d_output, input_size, special_node_loc)
-network.prepare(data_source=data_source, learning_rate=lin_reg_learning_rate,
+network.prepare(data_source=None, learning_rate=lin_reg_learning_rate,
                 iterations=lin_reg_iterations)
-optimizer = SingleSiteMPSOptimizer(network, max_size, optimizer_parameters)
-optimizer.train(data_source, batch_size, n_step,
-                training_parameters)
+# Load weights
+with open('weights', 'rb') as fp:
+    weights = pickle.load(fp)
+    if len(weights) != input_size:
+        weights = None
 
+# weights=None
+# optimizer_parameters = MPSOptimizerParameters(cutoff=cutoff, reg=reg, lr_reg=lr_reg,
+#                                               verbosity=verbosity, armijo_iterations=armijo_iterations)
+# training_parameters = MPSTrainingParameters(rate_of_change=rate_of_change, initial_weights=weights,
+#                                             _logging_enabled=logging_enabled)
+# optimizer = SingleSiteMPSOptimizer(network, max_size, optimizer_parameters)
+# optimizer.train(data_source, batch_size, n_step,
+#                 training_parameters)
+
+# Testing
+
+feed_dict = network.create_feed_dict(weights)
+test_features, test_labels = data_source.test_data
+features = tf.placeholder(tf.float32, shape=[input_size, None, d_feature])
+labels = tf.placeholder(tf.float32, shape=[None, d_output])
+f = network.predict(features)
+confusion_matrix = network.confusion_matrix(f, labels)
+accuracy = network.accuracy(f, labels)
+cost = network.cost(f, labels)
+feed_dict[features] = test_features
+feed_dict[labels] = test_labels
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    conf, acc, test_f, test_cost = sess.run([confusion_matrix, accuracy, f, cost], feed_dict = feed_dict)
+print("\n\n\n\n Accuracy is:" + str(acc) + " and cost is:" + str(test_cost))
+print("\n\n\n\n" + str(conf))
+print("\n\n\n\n Sample prediction: " + str(test_f[1]))
+print("Correct prediction for this case is: " + str(test_labels[1]))
