909230873b15926ae03c1ad3e5993082123fd1fa
Taketomo <ti250@cam.ac.uk>
2017-09-11 21:58:53 +0900

    Modified training scripts to use the new file format

--- trmps/FashionMNISTTraining.py
+++ trmps/FashionMNISTTraining.py
@@ -33,18 +33,19 @@ verbosity = -0
 
 cutoff = 100
 n_step = 6
+weights = None
 
 data_source = FashionMNISTDatasource(shrink=shrink, permuted=permuted, shuffled=shuffled)
+
+# Create network from scratch
 network = sqMPS(d_feature, d_output, input_size, special_node_loc)
 network.prepare(data_source=None, learning_rate=lin_reg_learning_rate,
                 iterations=lin_reg_iterations)
-# Load weights
-with open('weights', 'rb') as fp:
-    weights = pickle.load(fp)
-    if len(weights) != input_size:
-        weights = None
 
-# weights=None
+# Load network from saved configuration
+# network = MPS.from_file()
+
+# Training
 # optimizer_parameters = MPSOptimizerParameters(cutoff=cutoff, reg=reg, lr_reg=lr_reg,
 #                                               verbosity=verbosity, armijo_iterations=armijo_iterations)
 # training_parameters = MPSTrainingParameters(rate_of_change=rate_of_change, initial_weights=weights,
@@ -54,21 +55,4 @@ with open('weights', 'rb') as fp:
 #                 training_parameters)
 
 # Testing
-
-feed_dict = network.create_feed_dict(weights)
-test_features, test_labels = data_source.test_data
-features = tf.placeholder(tf.float32, shape=[input_size, None, d_feature])
-labels = tf.placeholder(tf.float32, shape=[None, d_output])
-f = network.predict(features)
-confusion_matrix = network.confusion_matrix(f, labels)
-accuracy = network.accuracy(f, labels)
-cost = network.cost(f, labels)
-feed_dict[features] = test_features
-feed_dict[labels] = test_labels
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    conf, acc, test_f, test_cost = sess.run([confusion_matrix, accuracy, f, cost], feed_dict = feed_dict)
-print("\n\n\n\n Accuracy is:" + str(acc) + " and cost is:" + str(test_cost))
-print("\n\n\n\n" + str(conf))
-print("\n\n\n\n Sample prediction: " + str(test_f[1]))
-print("Correct prediction for this case is: " + str(test_labels[1]))
+network.test(data_source.test_data[0], data_source.test_data[1])
--- trmps/cardioTraining.py
+++ trmps/cardioTraining.py
@@ -36,49 +36,18 @@ print(data_source.num_train_samples, data_source.num_test_samples)
 weights = None
 
 # DMRG optimizer
-
-with open('weights_sgd', 'rb') as fp:
-    weights = pickle.load(fp)
-    if len(weights) != input_size:
-        weights = None
-training_parameters.initial_weights = weights
-
+# Create network from scratch
 network = sqMPS(d_feature, d_output, input_size,
                 special_node_loc=special_node_loc)
 network.prepare(data_source, lin_reg_iterations)
+
+# Load network from saved configuration
+# network = MPS.from_file()
+
 optimizer = MPSOptimizer(network, max_size, optimizer_parameters)
 optimizer.train(data_source, batch_size, n_step,
                 training_parameters)
 
-# SGD optimizer
-
-# batch_size = int(data_source.num_train_samples / 10)
-# network = SimpleMPS(d_feature, d_output, input_size,
-#                     special_node_loc=special_node_loc, reg=reg)
-# network.prepare(data_source, lin_reg_iterations)
-# optimizer = SGDOptimizer(network)
-# optimizer.train(data_source, batch_size, n_step,
-#                 rate_of_change=rate_of_change)
-
 # Testing
+# network.test(data_source.test_data[0], data_source.test_data[1])
 
-# with open('weights', 'rb') as fp:
-#    weights = pickle.load(fp)
-#    if len(weights) != input_size:
-#        weights = None
-#
-# network.prepare(data_source)
-# feed_dict = network.create_feed_dict(weights)
-# test_features, test_labels = data_source.test_data
-# features = tf.placeholder(tf.float32, shape=[input_size, None, d_feature])
-# labels = tf.placeholder(tf.float32, shape=[None, d_output])
-# f = network.predict(features)
-# confusion_matrix = network.confusion_matrix(f, labels)
-# accuracy = network.accuracy(f, labels)
-# feed_dict[features] = test_features
-# feed_dict[labels] = test_labels
-# with tf.Session() as sess:
-#     sess.run(tf.global_variables_initializer())
-#     conf, acc = sess.run([confusion_matrix, accuracy], feed_dict = feed_dict)
-# print("\n\n\n\n Accuracy is:" + str(acc))
-# print("\n\n\n\n" + str(conf))
