8fd597dfd9d7041c5e85deb04a1f24ebae981b9d
Taketomo <ti250@cam.ac.uk>
2017-09-30 11:36:11 +0100

    Changed training script for slightly better performance

--- trmps/MNISTTraining.py
+++ trmps/MNISTTraining.py
@@ -5,7 +5,7 @@ from trmps import *
 d_feature = 2
 d_output = 10
 input_size = 784
-lin_reg_learning_rate = 10**(-4)
+lin_reg_learning_rate = 10**(-1)
 
 # Data parameters
 permuted = False
@@ -14,10 +14,10 @@ shrink = True
 if shrink:
     input_size = 196
 
-special_node_loc = 98
+special_node_loc = None
 
 # Optimizer parameters
-batch_size = 2000
+batch_size = 10000
 max_size = 30
 min_singular_value = 0.001
 reg = 0.01
@@ -33,24 +33,25 @@ cutoff = 100
 n_step = 6
 
 data_source = MNISTpreprocessing.MNISTDatasource(shrink=shrink, permuted=permuted, shuffled=shuffled)
+# data_source._save_binary()
 
-weights=None
+weights = None
 
 optimizer_parameters = MPSOptimizerParameters(cutoff=cutoff, reg=reg, lr_reg=lr_reg,
                                               verbosity=verbosity)
 training_parameters = MPSTrainingParameters(rate_of_change=rate_of_change, initial_weights=weights,
                                             _logging_enabled=logging_enabled)
 # Create network from scratch
-network = sqMPS(d_feature, d_output, input_size, special_node_loc)
-network.prepare(data_source=data_source, learning_rate=lin_reg_learning_rate)
+# network = MPS(d_feature, d_output, input_size, special_node_loc)
+# network.prepare(data_source=data_source, learning_rate=lin_reg_learning_rate, iterations=10000)
 
 # Load network from saved configuration
-# network = MPS.from_file()
+network = MPS.from_file()
 
 # Training
-optimizer = MPSOptimizer(network, max_size, optimizer_parameters)
-optimizer.train(data_source, batch_size, n_step,
-                training_parameters)
+# optimizer = SingleSiteMPSOptimizer(network, max_size, optimizer_parameters)
+# optimizer.train(data_source, batch_size, n_step,
+#                 training_parameters)
 
 # Testing
-# network.test(data_source.test_data[0], data_source.test_data[1])
+network.test(data_source.test_data[0], data_source.test_data[1])
