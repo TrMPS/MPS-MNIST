421b4532ea72da8b0da037625e0104c1a5c8d57d
Taketomo <ti250@cam.ac.uk>
2018-03-28 11:30:03 +0300

    Added a bisection solver
    The newton raphson solver would sometimes not converge, so a bisection
    solver is now used in its place

--- trmps/samplers.py
+++ trmps/samplers.py
@@ -1,16 +1,16 @@
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-from nrsolver import _nr_solver, _generate_dist_funcs
+from solvers import _bisection_solver, _generate_dist_funcs
 
 def quad_sample(a, b, n, name='quadratic', seed=None):
     norm = a + b
     _a = a / norm
     _b = b / norm
     ys = tf.random_uniform([tf.shape(a)[0]], minval=0, maxval=1, seed=seed)
-    cdfs, pdfs = _generate_dist_funcs(_a, _b, ys)
+    cdfs, _ = _generate_dist_funcs(_a, _b, ys)
     x_dim = tf.reshape(tf.shape(a)[0], [1])
-    return _nr_solver(cdfs, pdfs, x_dim, n=n)
+    return _bisection_solver(cdfs, x_dim, n=n)
 
 def _quad_sample_test(a, b, name='quadratic', seed=None):
     norm = a + b
@@ -22,7 +22,7 @@ def _quad_sample_test(a, b, name='quadratic', seed=None):
     print(x_dim.shape)
     print(a.shape)
     x_dim = tf.Print(x_dim, [x_dim])
-    xs = _nr_solver(cdfs, pdfs, x_dim, n=5)
+    xs = _bisection_solver(f=cdfs, x_dim=x_dim, n=10)
     return ys, xs, pdfs(xs)
 
 
@@ -38,19 +38,19 @@ if __name__ == '__main__':
     a = tf.constant(a_nparray)
     b = tf.constant(b_nparray)
 
-    cdfs, samples, cdfs_inv = _quad_sample_test(a, b, )
+    cdfs, samples, cdfs_inv = _quad_sample_test(a, b)
 
     with tf.Session() as sess:
         tf.global_variables_initializer()
         samples, cdfs, cdfs_inv = sess.run([samples, cdfs, cdfs_inv])
         print(samples)
         print(cdfs)
-        plt.plot(samples, cdfs, 'k.')
-        plt.plot(samples, cdfs_inv, 'b.')
         print(cdfs_inv)
         print(cdfs - cdfs_inv)
         print(samples[samples > 1].shape)
         print(cdfs[cdfs < 0.5].shape)
+        plt.plot(samples, cdfs, 'k.')
+        plt.plot(samples, cdfs_inv, 'b.')
         plt.show()
 
 
---
+++ trmps/solvers.py
@@ -0,0 +1,81 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+def _nr_solver(f, df, x_dim, n=10):
+    # solves in boundary [0,1]
+    # n is the number of iterations
+    def _nr_solver_helper(counter, x):
+        return counter + 1, tf.floormod(x - (f(x) / df(x)), 1.0)
+
+    cond = lambda c, *args: tf.less(c, n)
+    x_initial = tf.zeros(x_dim) + 1.0
+    x_initial = tf.Print(x_initial, [x_dim])
+    _, _x = tf.while_loop(cond=cond,
+                          body=_nr_solver_helper,
+                          loop_vars=[0, x_initial],
+                          # shape_invariants=(tf.TensorShape([]),
+                          #                  tf.TensorShape([])),
+                          parallel_iterations=1)
+    return _x
+
+
+def _bisection_solver(f, x_dim, n=9):
+    # solves in boundary [0,1]
+    # n is the number of iterations
+    def _bisection_solver_helper(counter, a, b):
+        midpoint = (a + b) / 2
+        f_mid = f(midpoint)
+        f_mid = tf.Print(f_mid, [f_mid, midpoint], summarize=10)
+
+        # def _new_b_finder(_f_mid):
+        #     cond = tf.less(_f_mid, 0.0)
+        #     return tf.cast(cond, tf.float32)
+        # mid_mask = tf.map_fn(_new_b_finder, f_mid)
+        mid_mask = tf.cast(tf.less(f_mid, 0.0), tf.float32)
+        mid_mask_inverted = 1.0 - mid_mask
+        new_b_1 = mid_mask_inverted * midpoint
+        new_b_2 = mid_mask * b
+        new_b = new_b_1 + new_b_2
+        new_b = tf.Print(new_b, [mid_mask, new_b, b], summarize=10)
+        new_a_1 = mid_mask_inverted * a
+        new_a_2 = mid_mask * midpoint
+        new_a = new_a_1 + new_a_2
+        return counter + 1, new_a, new_b
+
+    cond = lambda c, *args: tf.less(c, n)
+    a_initial = tf.zeros(x_dim)
+    b_initial = tf.zeros(x_dim) + 1.0
+    _, a, b = tf.while_loop(cond=cond,
+                          body=_bisection_solver_helper,
+                          loop_vars=[0, a_initial, b_initial],
+                          # shape_invariants=(tf.TensorShape([]),
+                          #                  tf.TensorShape([])),
+                          parallel_iterations=1)
+    return a
+
+def _test_f(x):
+    return (x + 3) * (x - 5) * (x - .6)
+
+
+def _test_df(x):
+    return 3.0 * (x ** 2 - 1.5 * x - 4.83333333)
+
+
+def _generate_dist_funcs(a, b, offsets):
+    def f(x):
+        return a * x + b * (3 * x - 6 * x**2 + 4 * x**3) - offsets
+
+    def df(x):
+        return a + 3 * b * (2 * x - 1)**2
+    return f, df
+
+
+if __name__ == '__main__':
+    result = bisection_solver(_test_f, 1, n=10)
+    # This test doesn't currently work because of the way the test function starts positive and goes negative, but this isn't a problem for the intended use case of the bisection_solver
+    with tf.Session() as sess:
+        tf.global_variables_initializer()
+        computed_result = sess.run([result])
+        print(computed_result)
